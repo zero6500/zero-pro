b6f60a6f9bfd04c36e4abbde3eb3946476db6175
zero6500 <zeroakira00@gmail.com>
2024-12-19 03:08:03 +0100

    Update Criticals.h
    1

--- Horion/Module/Modules/Criticals.h
+++ Horion/Module/Modules/Criticals.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "Module.h"
-class Criticals : public IModule {
-public:
-	Criticals();
-	~Criticals();
+#include <chrono> // لإضافة منطق الزمن
 
-	// Inherited via IModule
-	virtual const char* getModuleName() override;
-	virtual void onSendPacket(C_Packet* packet) override;
+class Criticals : public IModule {
+private:
+    std::chrono::steady_clock::time_point lastCriticalTime; // لتسجيل وقت آخر ضربة
+
+public:
+    Criticals();
+    ~Criticals();
+
+    // Inherited via IModule
+    virtual const char* getModuleName() override;
+    virtual void onSendPacket(C_Packet* packet) override;
 };
